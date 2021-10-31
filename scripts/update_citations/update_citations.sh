7270bc26266da77c5ea51db9ff8c1df5e8175181
czarnewski <paulo.czarnewski@scilifelab.se>
2021-10-31 13:35:04 +0100

    updates citations (sön 31 okt 2021 13:35:04 CET)

--- scripts/update_citations/update_citations.sh
+++ scripts/update_citations/update_citations.sh
@@ -4,8 +4,8 @@ git pull
 cd scripts/update_citations
 python3 ~/repos/webscraping/code/google_scholar/google_scholar.py
 
-sed -z "s|<div id=\"citations\">.*</div>|<div id=\"citations\">\nMYTAG\n</div>|g" ../../publications.md | sed -e "/MYTAG/r citations.csv" -e "//d"
+sed -z "s|<div id=\"citations\">.*</div>|<div id=\"citations\">\nMYTAG\n</div>|g" ~/repos/czarnewski/publications.md | sed -e "/MYTAG/r citations.csv" -e "//d"
 
 git add .
-git commit -m "$(echo 'updates citations' `date`)"
+git commit -m "$(echo 'updates citations ('`date`')')"
 git push
